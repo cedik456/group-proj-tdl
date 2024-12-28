3e78fc582b41e30feb5b5913131fb56d7ffa413a
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2024-12-28 14:42:37 +0800

    Added functions of Todo List w/ TW

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Trash2, CheckCircle, Circle} from "lucide-react";
+
+function TaskItem ({ task, onToggleComplete, onDelete }){
+    return (
+        <div>
+            <div>
+                <button onClick={
+                    () => onToggleComplete(task.id)
+                } className="focus:outline-none">
+                    {task.completed ? ( <CheckCircle className="text-green-500" size={20} /> ) : ( <Circle className="text-gray-400" size={20} /> )}
+                </button>
+                <span className={`${task.completed ? 'line-through text-gray-500' : ''}`} >
+                    {task.text}
+                </span>
+            </div>
+
+            <button onClick={() => onDelete(task.id)} className="text-red-500 hover:text-red-600 focus:outline-none" >
+                <Trash2 size={20} />
+            </button>
+        </div>
+    );
+};
+
+export default TaskItem;
