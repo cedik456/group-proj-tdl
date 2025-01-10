2614633dc7b0d1677c4f04165568cfacbb579904
Lou <cannojasperlew@gmail.com>
2025-01-11 01:53:09 +0800

    fuck alas dos na pala

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,19 @@
 import ProfilePic from "../assets/images/profile.png";
+import PropTypes from "prop-types";
 import { IoIosArrowDown, IoIosAddCircle } from "react-icons/io";
-import { BsLayoutSidebar } from "react-icons/bs";
 import { GoBell } from "react-icons/go";
 import { MdInbox } from "react-icons/md";
 import { BiCalendarAlt } from "react-icons/bi";
 import { CgCalendarDates } from "react-icons/cg";
+import { BsLayoutSidebar } from "react-icons/bs";
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, setIsOpen }) => {
   return (
-    <nav className="fixed top-0 flex flex-col items-center h-screen px-4 bg-[#FCFAF8] w-72">
+    <nav
+      className={`fixed top-0 left-0 flex flex-col items-center h-screen px-4 bg-[#FCFAF8] transition-transform duration-300 ${
+        isOpen ? "translate-x-0" : "-translate-x-60"
+      } w-72`}
+    >
       <div className="w-full py-2">
         <div className="flex items-center justify-between mb-2">
           <div className="flex items-center gap-2">
@@ -22,7 +27,7 @@ const Sidebar = () => {
           </div>
           <div className="flex items-center gap-6 text-xl ">
             <GoBell />
-            <BsLayoutSidebar />
+            <BsLayoutSidebar onClick={() => setIsOpen(!isOpen)} />
           </div>
         </div>
         <button className="flex items-center w-full gap-2 py-2 pl-1 mb-3 hover:bg-slate-200 ">
@@ -55,6 +60,11 @@ const Sidebar = () => {
       </div>
     </nav>
   );
+};
+
+Sidebar.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  setIsOpen: PropTypes.func.isRequired,
 };
 
 export default Sidebar;
