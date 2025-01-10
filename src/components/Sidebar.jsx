c58fe412a78e5faa310011b27fff22f05bb2d818
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-10 23:07:58 +0800

    Finish sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,53 +5,97 @@ import { GoBell } from "react-icons/go";
 import { MdInbox } from "react-icons/md";
 import { BiCalendarAlt } from "react-icons/bi";
 import { CgCalendarDates } from "react-icons/cg";
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { Home } from "lucide-react";
 
 const Sidebar = () => {
   return (
-    <nav className="fixed top-0 flex flex-col items-center h-screen px-4 bg-[#FCFAF8] w-72">
-      <div className="w-full py-2">
-        <div className="flex items-center justify-between mb-2">
-          <div className="flex items-center gap-2">
-            <img
-              src={ProfilePic}
-              alt=""
-              className="w-6 h-6 border border-black rounded-full"
-            />
-            <h1 className="mt-4 text-lg font-normal">Ced</h1>
-            <IoIosArrowDown className="text-lg" />
+    <nav className="fixed top-0 flex flex-col items-center h-screen px-4 py-4 bg-[#FCFAF8] w-72">
+      <div className="flex flex-col flex-grow w-full gap-20 py-2">
+        <div>
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center gap-2">
+              <img
+                src={ProfilePic}
+                alt=""
+                className="w-6 h-6 border-2 rounded-full border-primary-600"
+              />
+              <span className="text-lg font-normal ">Ced</span>
+              <IoIosArrowDown className="text-lg" />
+            </div>
+            <div className="flex items-center gap-6 text-xl ">
+              <GoBell />
+              <BsLayoutSidebar />
+            </div>
           </div>
-          <div className="flex items-center gap-6 text-xl ">
-            <GoBell />
-            <BsLayoutSidebar />
+          <button className="flex items-center w-full gap-2 py-2 pl-1 mb-2 text-lg rounded-md text-primary-500 hover:bg-[#F1EFED] ">
+            <IoIosAddCircle className="text-3xl " />
+            Add Task
+          </button>
+          <div className="flex flex-col gap-2 text-base ">
+            <a
+              href=""
+              className="flex items-center gap-2 p-2 text-black rounded-md hover:bg-[#F1EFED] hover:text-current "
+            >
+              <MdInbox className="text-xl" />
+              Inbox
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 p-2 text-black rounded-md hover:bg-[#F1EFED] hover:text-current"
+            >
+              <BiCalendarAlt className="text-xl" />
+              Today
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 p-2 text-black rounded hover:bg-[#F1EFED] hover:text-current"
+            >
+              <CgCalendarDates className="text-xl" />
+              Upcoming
+            </a>
           </div>
         </div>
-        <button className="flex items-center w-full gap-2 py-2 pl-1 mb-3 hover:bg-slate-200 ">
-          <IoIosAddCircle className="text-2xl" />
-          Add Task
-        </button>
-        <div className="flex flex-col gap-2 ">
-          <a
-            href=""
-            className="flex items-center gap-2 p-2 text-black rounded hover:bg-slate-200"
-          >
-            <MdInbox className="text-lg" />
-            Inbox
-          </a>
-          <a
-            href=""
-            className="flex items-center gap-2 p-2 text-black rounded hover:bg-slate-200"
-          >
-            <BiCalendarAlt className="text-lg" />
-            Today
-          </a>
-          <a
-            href=""
-            className="flex items-center gap-2 p-2 text-black rounded hover:bg-slate-200"
-          >
-            <CgCalendarDates className="text-lg" />
-            Upcoming
-          </a>
+        <div>
+          <div>
+            <h1 className="text-lg font-normal">My Projects</h1>
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 p-2 text-base rounded-md hover:bg-[#F1EFED]">
+              <span>#</span>
+              <Link
+                to="/"
+                className="flex items-center gap-1 text-black hover:text-current"
+              >
+                Home
+              </Link>
+            </div>
+
+            <div className="flex items-center gap-2 p-2 text-base rounded-md hover:bg-[#F1EFED]">
+              <span>#</span>
+              <Link
+                to="/"
+                className="flex items-center gap-1 text-black hover:text-current"
+              >
+                My Work
+              </Link>
+            </div>
+
+            <div className="flex items-center gap-2 p-2 text-base rounded-md hover:bg-[#F1EFED]">
+              <span>#</span>
+              <Link
+                to="/"
+                className="flex items-center gap-1 text-black hover:text-current"
+              >
+                Education
+              </Link>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="text-xs text-left text-primary-500">
+        © 2025 | Ahead | All Rights Reserved.
       </div>
     </nav>
   );
