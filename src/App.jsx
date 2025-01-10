b575ea2c07838bed188ebe98608b4810e72d9efc
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-01-10 18:05:21 +0800

    Clean out all the unnecessary codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import React, { useState } from "react";
-import TaskInput from "./components/TaskInput";
-import TaskList from "./components/TaskList";
-// import TaskList from "./components/TaskList";
-
 function App() {
   return (
-    <div className="container mt-[150px] items-center flex justify-center mx-auto">
-      <div className="card">
-        <TaskInput />
-        <TaskList />
-      </div>
-    </div>
+    <div className="container mt-[150px] items-center flex justify-center mx-auto"></div>
   );
 }
 
